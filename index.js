ab9a7a517c44330f0aa05a96893a53238860eef4
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-04-02 00:16:56 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+ 
+    return fetch("https://anapioficeandfire.com/api/books")
+      .then((response) => response.json())
+      .then((data) => renderBooks(data))
+      .catch((error) => console.log("Error fetching books:", error));
+  
   
 }
 
